59d01a460e6d8a03f21b9b4a8557ca0af52ffe82
JiangTao <565038254@qq.com>
2023-09-21 20:03:21 +0800

    搭建Oauth2.0安全认证服务配置集成 JWT认证

--- polaris-gateway/src/main/java/com/polaris/config/SecurityConfig.java
+++ polaris-gateway/src/main/java/com/polaris/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.polaris.config;
 
 import com.polaris.manager.AccessManager;
 import com.polaris.manager.ReactiveJdbcAuthenticationManager;
+import com.polaris.manager.ReactiveJwtAuthenticationManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,7 +10,10 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.ReactiveAuthenticationManager;
 import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 import org.springframework.security.oauth2.server.resource.web.server.ServerBearerTokenAuthenticationConverter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.authentication.AuthenticationWebFilter;
@@ -26,11 +30,25 @@ public class SecurityConfig {
     @Autowired
     private AccessManager accessManager;
 
+    @Bean
+    public TokenStore tokenStore() {
+        return new JwtTokenStore(jwtTokenEnhancer());
+    }
+
+    @Bean
+    public JwtAccessTokenConverter jwtTokenEnhancer(){
+        JwtAccessTokenConverter jwtTokenEnhancer = new JwtAccessTokenConverter();
+        jwtTokenEnhancer.setSigningKey("polaris");
+        return jwtTokenEnhancer;
+    }
 
     @Bean
     SecurityWebFilterChain webFluxSecurityFilterChain(ServerHttpSecurity http) throws Exception{
         //token管理器
-        ReactiveAuthenticationManager tokenAuthenticationManager = new ReactiveJdbcAuthenticationManager(new JdbcTokenStore(dataSource));
+//        ReactiveAuthenticationManager tokenAuthenticationManager = new ReactiveJdbcAuthenticationManager(new JdbcTokenStore(dataSource));
+        //token管理器 jwt
+        ReactiveAuthenticationManager tokenAuthenticationManager
+                = new ReactiveJwtAuthenticationManager(tokenStore());
         //认证过滤器
         AuthenticationWebFilter authenticationWebFilter = new AuthenticationWebFilter(tokenAuthenticationManager);
         authenticationWebFilter.setServerAuthenticationConverter(new ServerBearerTokenAuthenticationConverter());
--- polaris-oauth/src/main/java/com/polaris/config/AuthorizationServerConfig.java
+++ polaris-oauth/src/main/java/com/polaris/config/AuthorizationServerConfig.java
@@ -4,6 +4,7 @@ import com.polaris.service.UserDetailServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -14,8 +15,11 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Aut
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.ClientDetailsService;
 import org.springframework.security.oauth2.provider.client.JdbcClientDetailsService;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 import javax.sql.DataSource;
 
@@ -40,7 +44,22 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
      */
     @Bean
     public TokenStore tokenStore() {
-        return new JdbcTokenStore(dataSource);
+        //数据库存储token
+//        return new JdbcTokenStore(dataSource);
+        //JWT token
+        return new JwtTokenStore(jwtTokenEnhancer());
+    }
+
+    /**
+     * JwtAccessTokenConverter
+     * TokenEnhancer的子类，帮助程序在JWT编码的令牌值和OAuth身份验证信息之间进行转换。
+     */
+    @Bean
+    public JwtAccessTokenConverter jwtTokenEnhancer(){
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        // 设置对称签名
+        converter.setSigningKey("polaris");
+        return converter;
     }
 
     /**
@@ -60,15 +79,30 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
         return new BCryptPasswordEncoder();
     }
 
+    @Primary
+    @Bean
+    public DefaultTokenServices tokenServices(){
+        DefaultTokenServices tokenServices = new DefaultTokenServices();
+        tokenServices.setTokenEnhancer(jwtTokenEnhancer());
+        tokenServices.setTokenStore(tokenStore());
+        tokenServices.setSupportRefreshToken(true);
+        //设置token有效期，默认12小时，此处修改为6小时   21600
+        tokenServices.setAccessTokenValiditySeconds(60);
+        //设置refresh_token的有效期，默认30天，此处修改为7天
+        tokenServices.setRefreshTokenValiditySeconds(60);
+        return tokenServices;
+    }
     /**
      * 认证服务器Endpoints配置
      */
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
         //如果需要使用refresh_token模式则需要注入userDetailService
-        endpoints.userDetailsService(userDetailService);
-        endpoints.authenticationManager(this.authenticationManager);
-        endpoints.tokenStore(tokenStore());
+        endpoints.authenticationManager(this.authenticationManager).
+        userDetailsService(userDetailService).
+        tokenStore(tokenStore()).
+        accessTokenConverter(jwtTokenEnhancer())
+                .tokenServices(tokenServices());
     }
 
     /**
